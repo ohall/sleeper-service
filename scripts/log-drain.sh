dc43fabf08a6ec37c81eb6a8979f7743b277ef68
ohall <ohall@grassroots-tech.com>
2025-01-25 06:54:30 -0500

    log drain

--- scripts/log-drain.sh
+++ scripts/log-drain.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
+ACTION=${1:-add}
 source .env
-heroku drains:add https://$OPEN_OBSERVE_USER_ID:$OPEN_OBSERVE_API_KEY@api.openobserve.ai/api/$OPEN_OBSERVE_ORG_ID/$OPEN_OBSERVE_STREAM_NAME/_json -a sleeper-service
-
+heroku drains:$ACTION https://$OPEN_OBSERVE_USER_ID:$OPEN_OBSERVE_API_KEY@api.openobserve.ai/api/$OPEN_OBSERVE_ORG_ID/$OPEN_OBSERVE_STREAM_NAME/_json -a sleeper-service
 heroku drains -a sleeper-service
-
