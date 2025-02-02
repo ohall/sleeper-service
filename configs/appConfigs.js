4a399b705ad5aac9c717b17f7db3c3dc8838c26f
ohall <ohall@grassroots-tech.com>
2025-02-01 19:56:45 -0500

    Update weekly menu Slack channel configuration

--- configs/appConfigs.js
+++ configs/appConfigs.js
@@ -2,7 +2,7 @@ export const appConfigs = {
   dbName: "meal_planner",
   dislikedMealsCollection: "disliked_meals",
   weeklyMealsCollection: "weekly_meals",
-  weeklyMenuChannel: "bot-test",
+  weeklyMenuChannel: "weekly-menu",
   slackWorkspace: "riverdogworkspace",
   slackTeamId: "T0880LG4KPS",
 };
