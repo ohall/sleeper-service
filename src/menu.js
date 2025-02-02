e067b7f3114afdfe427070b9ddcd0e59cb27b7e9
ohall <ohall@grassroots-tech.com>
2025-02-01 19:22:58 -0500

    Simplify loading state block in menu shopping list creation

--- src/menu.js
+++ src/menu.js
@@ -64,12 +64,7 @@ const handleRecordRecipes = async (channel, messageTs) => {
   await slack.client.chat.update({
     channel: channel,
     ts: messageTs,
-    blocks: [
-      {
-        type: "section",
-        text: { type: "plain_text", text: "Creating Shopping List" },
-      },
-    ],
+    blocks: [{ type: "section", text: { type: "plain_text", text: "Creating Shopping List" } }],
   });
   
   // Get this week's meals from MongoDB
